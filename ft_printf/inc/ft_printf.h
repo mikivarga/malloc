bcdbf6a3b6eeefc46c719c7d233fabc41db6208b
Mikhailo Varga <mvarga@e1r11p7.unit.ua>
2018-12-16 18:04:07 +0200

    added makefile

--- ft_printf/inc/ft_printf.h
+++ ft_printf/inc/ft_printf.h
@@ -13,7 +13,7 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include "libft.h"
+# include "../../libft/inc/libft.h"
 # include <stdarg.h>
 # include <stdint.h>
 # include <wchar.h>
---
+++ ft_printf/main.c
@@ -0,0 +1,8 @@
+#include "inc/ft_printf.h"
+
+int main(void)
+{
+    ft_printf("IDIOT MIKI!\n");
+
+    return 0;
+}
